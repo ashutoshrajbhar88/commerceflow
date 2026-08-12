c2408946fb123c7232c66cdd46eff2896f4cb541
ashutoshrajbhar88 <ashutoshrajbhar88@gmail.com>
2026-08-13 00:25:36 +0530

    initialize CommerceFlow Spring Boot project

---
+++ src/main/java/com/commerceflow/CommerceflowApplication.java
@@ -0,0 +1,13 @@
+package com.commerceflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceflowApplication.class, args);
+	}
+
+}
